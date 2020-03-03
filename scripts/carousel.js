ebba5a643d6476b3c4cb559b9b43edf580e1a775
DAVE <dave.bryant1@ntlworld.com>
2020-03-03 20:52:06 +0000

    0056

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -6,10 +6,10 @@
             speed: 1000,
             interval: 4000
         }
+        var position = 0;
         settings.itemWidth = parseInt($('div').find('.carousel-item').css("width"));
-
-        timer = setInterval(function () { slide(1); }, settings.interval);
-
+        var timer = null;
+        
         $('div').find('.mprev').on('click', function () {
             slide(-1);
         });
@@ -21,27 +21,46 @@
             window.location = $(this).data("href");
         });
 
+
+        $(".carousel-utility").on('mouseenter', function () {
+            clearInterval(timer);
+        }).on('mouseleave', function () {
+            autoPlay();
+        });
+
+        $(window).resize(function () {
+            settings.itemWidth = parseInt($('div').find('.carousel-item').css("width"));
+            slide(1);
+        });
+
+        autoPlay();
+
+
+        function autoPlay() {
+            timer = setInterval(function () { slide(1); }, settings.interval);
+        }
+
         function slide(x) {
-            var currentLeft = parseInt($('div').find('.carousel').position().left / settings.itemWidth);
-            var newLeft;
-            if ((currentLeft + settings.itemCount == 1) && (x == 1)) {
-                newLeft = 0;
+            if ((position - settings.itemCount == -1) && (x == 1)) {
+                position = 0;
             } else {
-                if ((x == -1) && (currentLeft == 0)) {
-                    newLeft = 1 - settings.itemCount;
+                if ((x == -1) && (position == 0)) {
+                    position = 3;
                 }
                 else {
-                    newLeft = (currentLeft - x);
+                    position = (position + x);
                 }
 
             }
-            var newPX = newLeft * settings.itemWidth;
+            var newPX = position * settings.itemWidth * -1;
             var state = { $zIndex: 1, width: 100, height: 100, top: 40, left: 375, $opacity: 0.9 };
             $('div').find('.carousel').finish().animate({ left: newPX + "px" }, settings.speed);
-
+            autoplay();
         }
 
-});
+    });
+
+
 
 
 
